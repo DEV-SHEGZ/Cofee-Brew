acaa172cff8fa50b9d0254e8718d7822a2d246ed
DEV-SHEGZ <strexy1998@gmail.com>
2025-10-20 14:40:07 +0100

    changed our products to varieties due to UI naming system, also done with the varieties page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
 import Discover from "./components/Discover.jsx";
 import CoffeeItems from "./components/Menu.jsx";
-import Products from "./components/Products.jsx";
+import Varieties from "./components/Varieties.jsx";
 function App() {
   return (
     <>
@@ -10,7 +10,7 @@ function App() {
       <Hero />
       <Discover />
       <CoffeeItems />
-      <Products />
+      <Varieties />
     </>
   );
 }
---
+++ src/components/Varieties.jsx
@@ -0,0 +1,62 @@
+import "../App.css";
+import Cappuccino from "../assets/cappuccino.png";
+import Dalgona from "../assets/Dalgona coffee.png";
+import ColdCoffee from "../assets/Cold coffee.png";
+import Filtercoffee from "../assets/filter coffee.png";
+
+const Varieties = () => {
+  return (
+    <div div className="varieties-section">
+      <p className="section-title">
+        Explore Our Delicious <span className="span-title">Coffee</span>{" "}
+        Creations
+      </p>
+      <div div className="products-container">
+        <div className="product-card">
+          <img className="product-img" src={Cappuccino} alt="" />
+          <p className="card-title">Cappuccino</p>
+          <p className="card-p">An espresso prepared with steamed milk</p>
+
+          <div className="card-price">
+            <p>$1.50</p>
+            <button className="product-btn">Order now</button>
+          </div>
+        </div>
+        <div className="product-card">
+          <img className="product-img" src={Dalgona} alt="" />
+          <p className="card-title">Dalgona Coffe</p>
+          <p className="card-p">An espresso prepared with steamed milk</p>
+
+          <div className="card-price">
+            <p>$1.50</p>
+            <button className="product-btn">Order now</button>
+          </div>
+        </div>
+        <div className="product-card">
+          <img className="product-img" src={ColdCoffee} alt="" />
+          <p className="card-title">Cold Coffee</p>
+          <p className="card-p">An espresso prepared with steamed milk</p>
+
+          <div className="card-price">
+            <p>$1.50</p>
+            <button className="product-btn">Order now</button>
+          </div>
+        </div>
+        <div className="product-card">
+          <img className="product-img" src={Filtercoffee} alt="" />
+          <p className="card-title">Filter Coffee</p>
+          <p className="card-p">An espresso prepared with steamed milk</p>
+
+          <div className="card-price">
+            <p>$1.50</p>
+            <button className="product-btn">Order now</button>
+          </div>
+        </div>
+      </div>
+
+      <button className="View-more-btn">View More</button>
+    </div>
+  );
+};
+
+export default Varieties;
