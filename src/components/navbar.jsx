2df9c1f17a7e7be4090ccf1cce3f4e6a8f55ab4c
DEV-SHEGZ <strexy1998@gmail.com>
2026-07-08 21:40:49 +0100

    still working mobile nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,30 +4,42 @@ import glass from "../assets/glass.png";
 import "../App.css";
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
+    <>
+      {/*Mobile-Nav*/}
+      <nav className="navbar-mobile">
         <img className="navbar-logo" src={navlogo} alt="Coffee Brew Logo" />
-        <p>CoffeeBrew</p>
-      </div>
-      <ul className="nav-ul">
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">Coffee Menu</a>
-        </li>
-        <li>
-          <a href="#services">About us</a>
-        </li>
-        <li>
-          <a href="#contact">Shop</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-      <img src={glass} alt="Coffee Glass" className="navbar-glass" />
-    </nav>
+        <div className="burger">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </nav>
+      {/*Desktop view*/}
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img className="navbar-logo" src={navlogo} alt="Coffee Brew Logo" />
+          <p>CoffeeBrew</p>
+        </div>
+        <ul className="nav-ul">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">Coffee Menu</a>
+          </li>
+          <li>
+            <a href="#services">About us</a>
+          </li>
+          <li>
+            <a href="#contact">Shop</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <img src={glass} alt="Coffee Glass" className="navbar-glass" />
+      </nav>
+    </>
   );
 };
 
